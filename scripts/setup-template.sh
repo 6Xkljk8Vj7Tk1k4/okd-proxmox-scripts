04fe958d4a85654aab8870693b7bf592c10f729a
Resident-Fortress <42253757+Resident-Fortress@users.noreply.github.com>
2024-08-11 16:31:59 -0500

    Update

--- scripts/setup-template.sh
+++ scripts/setup-template.sh
@@ -8,10 +8,9 @@ CPU=4
 RAM=16384
 NET_TYPE=virtio
 BRIDGE=vmbr0
-VLAN=20
 ADD_STORAGE=112G
 
-/usr/sbin/qm create $ID --name $NAME --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE,tag=$VLAN
+/usr/sbin/qm create $ID --name $NAME --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE
 /usr/sbin/qm importdisk $ID fedora-coreos.qcow2 $STORAGE
 /usr/sbin/qm set $ID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-$ID-disk-0
 /usr/sbin/qm set $ID --boot c --bootdisk scsi0
