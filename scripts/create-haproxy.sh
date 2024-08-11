f308fab53b8252307fc1461978089f2bb711ee32
Resident-Fortress <42253757+Resident-Fortress@users.noreply.github.com>
2024-08-11 16:04:50 -0500

    Modifed some more code

--- scripts/create-haproxy.sh
+++ scripts/create-haproxy.sh
@@ -4,18 +4,17 @@ ID=29998
 TEMPLATE_STORAGE=local
 TEMPLATE_NAME=debian-12-standard_12.2-1_amd64.tar.zst
 BRIDGE=vmbr0
-# VLAN=20  This is unessecary and breaks the ability to even install HAproxy... -mdudki
 STORAGE=zfs-intel
 PATH=$(pwd)
 
 /usr/bin/pveam download $TEMPLATE_STORAGE $TEMPLATE_NAME
 
-/usr/sbin/pct create $ID $TEMPLATE_STORAGE:vztmpl/$TEMPLATE_NAME --cores 1 --hostname okd-haproxy --net0 name=eth0,bridge=$BRIDGE,ip=dhcp,tag=$VLAN,type=veth --ostype ubuntu --storage $STORAGE
+/usr/sbin/pct create $ID $TEMPLATE_STORAGE:vztmpl/$TEMPLATE_NAME --cores 1 --hostname okd-haproxy --net0 name=eth0,bridge=$BRIDGE,ip=dhcp,type=veth --ostype debian --storage $STORAGE
 /usr/sbin/pct start $ID
 
 /usr/sbin/pct exec $ID -- bash -c 'apt-get update | apt-get -y install curl gnupg2'
 /usr/sbin/pct exec $ID -- bash -c 'curl https://haproxy.debian.net/bernat.debian.org.gpg | apt-key add -'
-/usr/sbin/pct exec $ID -- bash -c 'echo deb http://haproxy.debian.net bookworm-backports-2.3 main | tee /etc/apt/sources.list.d/haproxy.list'
+/usr/sbin/pct exec $ID -- bash -c 'echo deb http://haproxy.debian.net bookworm-backports-2.8 main | tee /etc/apt/sources.list.d/haproxy.list'
 /usr/sbin/pct exec $ID -- bash -c 'apt-get update | apt-get -y install haproxy=2.8.\*'
 /usr/sbin/pct push $ID $PATH/extras/haproxy.cfg /etc/haproxy/haproxy.cfg
 /usr/sbin/pct exec $ID -- bash -c 'systemctl restart haproxy'
