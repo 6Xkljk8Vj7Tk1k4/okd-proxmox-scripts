e5865d398ff950bd9d7f07f39329c27798fd575e
Resident-Fortress <42253757+Resident-Fortress@users.noreply.github.com>
2024-08-11 15:57:02 -0500

    Updated

--- scripts/create-haproxy.sh
+++ scripts/create-haproxy.sh
@@ -10,9 +10,10 @@ PATH=$(pwd)
 
 /usr/bin/pveam download $TEMPLATE_STORAGE $TEMPLATE_NAME
 
-/usr/sbin/pct create $ID $TEMPLATE_STORAGE:vztmpl/$TEMPLATE_NAME --cores 1 --hostname okd-haproxy --net0 name=eth0,bridge=$BRIDGE,ip=dhcp,tag=$VLAN,type=veth --ostype debian --storage $STORAGE
+/usr/sbin/pct create $ID $TEMPLATE_STORAGE:vztmpl/$TEMPLATE_NAME --cores 1 --hostname okd-haproxy --net0 name=eth0,bridge=$BRIDGE,ip=dhcp,tag=$VLAN,type=veth --ostype ubuntu --storage $STORAGE
 /usr/sbin/pct start $ID
 
+/usr/sbin/pct exec $ID -- bash -c 'apt-get update'
 /usr/sbin/pct exec $ID -- bash -c 'apt-get install --no-install-recommends software-properties-common'
 /usr/sbin/pct exec $ID -- bash -c 'add-apt-repository ppa:vbernat/haproxy-2.8'
 /usr/sbin/pct exec $ID -- bash -c 'apt-get update | apt-get -y install haproxy=2.8.\*'
