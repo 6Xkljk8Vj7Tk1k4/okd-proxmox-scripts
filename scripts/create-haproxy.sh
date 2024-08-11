60f69158f60da0f53c9fbe29d542fd4346eb4130
Resident-Fortress <42253757+Resident-Fortress@users.noreply.github.com>
2024-08-11 15:32:49 -0500

    Updated to use Ubuntu 22.04 LXC

--- scripts/create-haproxy.sh
+++ scripts/create-haproxy.sh
@@ -2,7 +2,7 @@
 
 ID=29998
 TEMPLATE_STORAGE=local
-TEMPLATE_NAME=debian-12-standard_12.2-1_amd64.tar.zst
+TEMPLATE_NAME=ubuntu-22.04-standard_22.04-1_amd64.tar.zst
 BRIDGE=vmbr0
 VLAN=20
 STORAGE=zfs-intel
@@ -13,9 +13,8 @@ PATH=$(pwd)
 /usr/sbin/pct create $ID $TEMPLATE_STORAGE:vztmpl/$TEMPLATE_NAME --cores 1 --hostname okd-haproxy --net0 name=eth0,bridge=$BRIDGE,ip=dhcp,tag=$VLAN,type=veth --ostype debian --storage $STORAGE
 /usr/sbin/pct start $ID
 
-/usr/sbin/pct exec $ID -- bash -c 'apt-get update | apt-get -y install curl gnupg2'
-/usr/sbin/pct exec $ID -- bash -c 'curl https://haproxy.debian.net/bernat.debian.org.gpg | apt-key add -'
-/usr/sbin/pct exec $ID -- bash -c 'echo deb http://haproxy.debian.net bookworm-backports-2.8 main | tee /etc/apt/sources.list.d/haproxy.list'
+/usr/sbin/pct exec $ID -- bash -c 'apt-get install --no-install-recommends software-properties-common'
+/usr/sbin/pct exec $ID -- bash -c 'add-apt-repository ppa:vbernat/haproxy-2.8'
 /usr/sbin/pct exec $ID -- bash -c 'apt-get update | apt-get -y install haproxy=2.8.\*'
 /usr/sbin/pct push $ID $PATH/extras/haproxy.cfg /etc/haproxy/haproxy.cfg
 /usr/sbin/pct exec $ID -- bash -c 'systemctl restart haproxy'
