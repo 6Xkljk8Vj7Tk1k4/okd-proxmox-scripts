c0265b5fdb5f8c075fbc14dd23abb8990fe238f0
6Xkljk8Vj7Tk1k4 <6Xkljk8Vj7Tk1k4@users.noreply.github.com>
2024-11-16 10:19:42 +0100

    Update setup-template.sh

--- scripts/setup-template.sh
+++ scripts/setup-template.sh
@@ -3,16 +3,16 @@
 
 ID=29999
 NAME=fcos-template
-STORAGE=zfs-intel
+STORAGE=data
 CPU=4
 RAM=16384
 NET_TYPE=virtio
 BRIDGE=vmbr0
 ADD_STORAGE=112G
 
-/usr/sbin/qm create $ID --name $NAME --cpu cputype=x86-64-v2 --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE
+/usr/sbin/qm create $ID --name $NAME --cputype --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE
 /usr/sbin/qm importdisk $ID fedora-coreos.qcow2 $STORAGE
-/usr/sbin/qm set $ID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-$ID-disk-0
-/usr/sbin/qm set $ID --boot c --bootdisk scsi0
-/usr/sbin/qm resize $ID scsi0 +$ADD_STORAGE
+/usr/sbin/qm set $ID --scsihw virtio-scsi-single --virtio0 $STORAGE:vm-$ID-disk-0
+/usr/sbin/qm set $ID --boot c --bootdisk virtio0
+/usr/sbin/qm resize $ID virtio0 +$ADD_STORAGE
 /usr/sbin/qm template $ID
