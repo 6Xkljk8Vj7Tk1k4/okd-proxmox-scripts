e393f88dbadfab0555c3fac489fcc329450527ca
6Xkljk8Vj7Tk1k4 <6Xkljk8Vj7Tk1k4@users.noreply.github.com>
2024-11-16 11:11:37 +0100

    Update setup-template.sh

--- scripts/setup-template.sh
+++ scripts/setup-template.sh
@@ -10,7 +10,7 @@ NET_TYPE=virtio
 BRIDGE=vmbr0
 ADD_STORAGE=112G
 
-/usr/sbin/qm create $ID --name $NAME --cputype --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE
+/usr/sbin/qm create $ID --name $NAME --cpu cputype=x86-64-v2 --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE
 /usr/sbin/qm importdisk $ID fedora-coreos.qcow2 $STORAGE
 /usr/sbin/qm set $ID --scsihw virtio-scsi-single --virtio0 $STORAGE:vm-$ID-disk-0
 /usr/sbin/qm set $ID --boot c --bootdisk virtio0
